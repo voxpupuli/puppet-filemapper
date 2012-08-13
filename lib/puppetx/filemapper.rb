5d00d3180f3650a8ab0fe7853bebadbb5c206086
Adrien Thebo <adrien@puppetlabs.com>
2012-08-13 10:19:46 -0700

    Implement new filemapper instances

---
+++ lib/puppetx/filemapper.rb
@@ -0,0 +1,55 @@
+
+# Forward declaration
+module PuppetX; end
+
+module PuppetX::FileMapper
+
+  class << self
+
+    attr_reader :failed
+
+    def initvars
+      @mapped_files = {}
+      @failed       = false
+    end
+
+    initvars
+
+    # Returns all instances of the provider including this class.
+    #
+    # @return [Array<Puppet::Provider>]
+    def instances
+      # Validate that the methods required for prefetching are available
+      [:files_to_prefetch, :prefetch_file].each do |method|
+        unless self.respond_to? method
+          raise NotImplementedError, "#{self.name} has not implemented `self.#{method}`"
+        end
+      end
+
+      # Retrieve a list of files to prefetch, and cache a copy of a filetype
+      # for each one
+      files_to_prefetch.each do |file|
+        @mapped_files[file] = Puppet::Util::FileType.filetype(:flat).new(file)
+      end
+
+      provider_hashes = []
+      @mapped_files.each_pair do |filename, filetype|
+        provider_hashes.concat(prefetch_file(filename, filetype.read))
+      end
+
+      # Add the provider name to each one of the new provider instances
+      # and then generate them.
+      provider_hashes.map do |h|
+        h.merge!({:provider => self.name})
+        new(h)
+      end
+
+    rescue
+      # If something failed while loading instances, mark the provider class
+      # as failed and pass the exception along
+      self.failed = true
+      raise
+    end
+
+  end
+end
