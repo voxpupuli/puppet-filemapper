5e91959584d69f597c24491ef9e246dacf6b56a5
Adrien Thebo <adrien@puppetlabs.com>
2012-08-13 17:00:57 -0700

    Expand tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require 'mocha'
 
 PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 $LOAD_PATH.unshift(File.join(PROJECT_ROOT, "lib"))
+$LOAD_PATH.unshift(File.join(PROJECT_ROOT, "spec", "lib"))
 
 RSpec.configure do |config|
   config.mock_with :mocha
--- spec/unit/filemapper_spec.rb
+++ spec/unit/filemapper_spec.rb
@@ -3,6 +3,14 @@ require 'puppetx/filemapper'
 
 describe PuppetX::FileMapper do
 
+  before do
+    @ramtype  = Puppet::Util::FileType.filetype(:ram)
+    @flattype = Puppet::Util::FileType.filetype(:flat)
+
+    puts Puppet::Util::FileType.stub(:filetype).with(:flat)
+    puts Puppet::Util::FileType.stub(:filetype).with(:flat).and_return @ramtype
+  end
+
   let(:dummytype) do
     Puppet::Type.newtype(:dummy) do
       newparam(:name, :namevar => true)
@@ -29,11 +37,39 @@ describe PuppetX::FileMapper do
         dummytype.provide(:incomplete) { include PuppetX::FileMapper }
       end
 
-      it '' do
-        expect { subject.validate_class! }.to raise_error
-      end
+      it { expect { subject.validate_class! }.to raise_error Puppet::DevError, /target_files/ }
     end
 
+    describe "and it doesn't implement self.parse_file" do
+      subject do
+        dummytype.provide(:incomplete) do
+          include PuppetX::FileMapper
+          def self.target_files; end
+        end
+      end
+
+      it { expect { subject.validate_class! }.to raise_error Puppet::DevError, /parse_file/}
+    end
   end
 
+  describe 'when reading' do
+
+    describe 'a single file' do
+      subject do
+        dummytype.provide(:incomplete) do
+          include PuppetX::FileMapper
+          def self.target_files; ['/foo']; end
+          def self.parse_file(filename, content)
+            [{:name => 'yay', :foo => :bla, :bar => 'baz'}]
+          end
+        end
+      end
+
+      it 'should generate a filetype for each file' do
+        puts @flattype.methods.sort
+        @flattype.stub(:new).with('/foo').once.and_return @ramtype.new('/foo')
+        subject.load_all_providers_from_disk
+      end
+    end
+  end
 end
