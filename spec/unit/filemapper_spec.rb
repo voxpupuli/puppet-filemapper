0ee5ce22720f6ea500fe678801153edc76ea1567
Adrien Thebo <adrien@puppetlabs.com>
2012-08-14 10:04:31 -0700

    Expand test coverate for file reading

--- spec/unit/filemapper_spec.rb
+++ spec/unit/filemapper_spec.rb
@@ -51,10 +51,12 @@ describe PuppetX::FileMapper do
   end
 
   describe 'when reading' do
-
     describe 'a single file' do
+
+      let(:data) { [{:name => 'yay', :foo => :bla, :bar => 'baz'}] }
+
       subject do
-        dummytype.provide(:incomplete) do
+        dummytype.provide(:single) do
           include PuppetX::FileMapper
           def self.target_files; ['/foo']; end
           def self.parse_file(filename, content)
@@ -63,10 +65,61 @@ describe PuppetX::FileMapper do
         end
       end
 
-      it 'should generate a filetype for each file' do
-        puts @flattype.methods.sort
-        @flattype.stub(:new).with('/foo').once.and_return @ramtype.new('/foo')
+      it 'should generate a filetype for that file' do
+        @flattype.expects(:new).with('/foo').once.returns @ramtype.new('/foo')
         subject.load_all_providers_from_disk
+      end
+
+      it 'should parse each file' do
+        stub_file = stub(:read => 'file contents')
+        @flattype.stubs(:new).with('/foo').once.returns stub_file
+        subject.expects(:parse_file).with('/foo', 'file contents').returns []
+        subject.load_all_providers_from_disk
+      end
+
+      it 'should return the generated array' do
+        @flattype.stubs(:new).with('/foo').once.returns @ramtype.new('/foo')
+        subject.load_all_providers_from_disk.should == data
+      end
+    end
+
+    describe 'multiple files' do
+      subject do
+        dummytype.provide(:multiple) do
+          include PuppetX::FileMapper
+          def self.target_files; ['/bar', '/baz']; end
+          def self.parse_file(filename, content)
+            case filename
+            when '/bar' then [{:name => 'yay', :foo => :bla, :bar => 'baz'}]
+            when '/baz' then [{:name => 'whee', :foo => :ohai, :bar => 'wat'}]
+            end
+          end
+        end
+      end
+
+      it 'should generate a filetype for each file' do
+        @flattype.expects(:new).with('/bar').once.returns(stub(:read => 'barbar'))
+        @flattype.expects(:new).with('/baz').once.returns(stub(:read => 'bazbaz'))
+        subject.load_all_providers_from_disk
+      end
+
+      describe 'when parsing' do
+        before do
+          @flattype.stubs(:new).with('/bar').once.returns(stub(:read => 'barbar'))
+          @flattype.stubs(:new).with('/baz').once.returns(stub(:read => 'bazbaz'))
+        end
+
+        it 'should parse each file' do
+          subject.expects(:parse_file).with('/bar', 'barbar').returns []
+          subject.expects(:parse_file).with('/baz', 'bazbaz').returns []
+          subject.load_all_providers_from_disk
+        end
+
+        it 'should return the generated array' do
+          data = subject.load_all_providers_from_disk
+          data.should be_include({:name => 'yay', :foo => :bla, :bar => 'baz'})
+          data.should be_include({:name => 'whee', :foo => :ohai, :bar => 'wat'})
+        end
       end
     end
   end
